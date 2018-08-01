bfbab656dfdaf4b76e5bfec437ace8ebd49dfb14
Katheryn Osmond <katheryn.osmond@excella.com>
2018-08-01 14:21:06 +0000

    Ran migrations for Article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726163458) do
+ActiveRecord::Schema.define(version: 20180801141851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jokes", force: :cascade do |t|
     t.text "body"
